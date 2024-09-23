b1429f0e7945ef06cc7631209d0a85059b236f6c
J Boro <joepboro@gmail.com>
2024-09-23 14:09:13 +1000

    Add JwtBearer Authentication and allowed Origins

--- CineManage.API/Program.cs
+++ CineManage.API/Program.cs
@@ -1,22 +1,61 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOutputCache(options =>
+{
+    options.DefaultExpirationTimeSpan = TimeSpan.FromSeconds(60);
+});
+
+var allowedOrigins = builder.Configuration.GetValue<string>("AllowedOrigins")!.Split(",");
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins(allowedOrigins).AllowAnyMethod().
+            AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.Map("/mapTest", (app) =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("SHORT CIRCUITED!");
+    });
+});
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseOutputCache();
+
+// app.UseResponseCaching();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
